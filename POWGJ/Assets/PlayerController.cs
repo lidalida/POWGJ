b53a922b7bd88c06be50868c7b922ebb5f33dad5
lidalida <lidka.wojciechowska1996@gmail.com>
2016-10-08 17:04:27 +0200

    player controller

--- POWGJ/Assets/PlayerController.cs
+++ POWGJ/Assets/PlayerController.cs
@@ -3,16 +3,31 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+    public int speed = 3;
 	// Use this for initialization
 	void Start () {
+        
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-            GetComponent<Rigidbody2D>().velocity = Vector2.up*5;
-        else if(Input.GetKeyUp(KeyCode.UpArrow))
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0,0,0);
+       
+        if (Input.GetKey(KeyCode.UpArrow))
+            GetComponent<Rigidbody2D>().velocity = new Vector2(speed * Mathf.Sin(Mathf.Deg2Rad * GetComponent<Rigidbody2D>().rotation * (-1)), speed * Mathf.Cos(Mathf.Deg2Rad * GetComponent<Rigidbody2D>().rotation * (-1)));
+        if(Input.GetKeyUp(KeyCode.UpArrow))
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            GetComponent<Rigidbody2D>().rotation -= 5;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            GetComponent<Rigidbody2D>().rotation += 5;
+             
+
+
+        
     }
+
+    
 }
