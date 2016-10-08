2ea42ce3141b82a93a8a299ba6d88211e6a40179
Vonski <daniellodelapedro@interia.pl>
2016-10-08 14:14:28 +0200

    Working map, etc...

---
+++ POWGJ/Assets/DoorController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorController : MonoBehaviour {
+
+    bool isOpen;
+
+	// Use this for initialization
+	void Start () {
+        isOpen = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (isOpen)
+                transform.Rotate(Vector3.forward * 90);
+            else
+                transform.Rotate(Vector3.forward * (-90));
+            isOpen = !isOpen;
+        }
+           
+
+    }
+}
---
+++ POWGJ/Assets/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            GetComponent<Rigidbody2D>().velocity = Vector2.up*5;
+        else if(Input.GetKeyUp(KeyCode.UpArrow))
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0,0,0);
+    }
+}
