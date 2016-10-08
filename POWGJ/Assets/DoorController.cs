71577828d4a7a57d777fa7954b4798b4b8911c3d
Vonski <daniellodelapedro@interia.pl>
2016-10-08 19:57:47 +0200

    Working GUI, Working gameplay scene, working doors everywhere

--- POWGJ/Assets/DoorController.cs
+++ POWGJ/Assets/DoorController.cs
@@ -12,14 +12,15 @@ public class DoorController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            if (isOpen)
-                transform.Rotate(Vector3.forward * 90);
-            else
-                transform.Rotate(Vector3.forward * (-90));
-            isOpen = !isOpen;
-        }
+        if(Vector3.Distance(GameObject.Find("Player").transform.position,transform.position+Vector3.left*1.63f)<1.2f)
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (isOpen)
+                    transform.Rotate(Vector3.forward * 90);
+                else
+                    transform.Rotate(Vector3.forward * (-90));
+                isOpen = !isOpen;
+            }
            
 
     }
---
+++ POWGJ/Assets/GUIController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIController : MonoBehaviour {
+
+	public void ChangeScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+
+    public void TheEnd()
+    {
+        Application.Quit();
+    }
+}
