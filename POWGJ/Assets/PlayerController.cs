a4927bad4230d8ebe0a85d1dc06ea442cc6759e5
lidalida <lidka.wojciechowska1996@gmail.com>
2016-10-09 11:28:26 +0200

    player animation

--- POWGJ/Assets/PlayerController.cs
+++ POWGJ/Assets/PlayerController.cs
@@ -6,22 +6,37 @@ public class PlayerController : MonoBehaviour {
     public int speed = 3;
 	// Use this for initialization
 	void Start () {
+        GetComponent<Animator>().SetBool("IsMoving", false);
+
         
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-       
-        if (Input.GetKey(KeyCode.UpArrow))
-            GetComponent<Rigidbody2D>().velocity = new Vector2(speed * Mathf.Sin(Mathf.Deg2Rad * GetComponent<Rigidbody2D>().rotation * (-1)), speed * Mathf.Cos(Mathf.Deg2Rad * GetComponent<Rigidbody2D>().rotation * (-1)));
-        if(Input.GetKeyUp(KeyCode.UpArrow))
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
 
-        if (Input.GetKey(KeyCode.DownArrow))
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-speed * Mathf.Sin(Mathf.Deg2Rad * GetComponent<Rigidbody2D>().rotation*(-1)), -speed * Mathf.Cos(Mathf.Deg2Rad * GetComponent<Rigidbody2D>().rotation*(-1)));
-        if (Input.GetKeyUp(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(speed * Mathf.Sin(Mathf.Deg2Rad * GetComponent<Rigidbody2D>().rotation * (-1)), speed * Mathf.Cos(Mathf.Deg2Rad * GetComponent<Rigidbody2D>().rotation * (-1)));
+            GetComponent<Animator>().SetBool("IsMoving", true);
+            Debug.Log("true");
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
             GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            GetComponent<Animator>().SetBool("IsMoving", false);
+            Debug.Log("false");
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-speed * Mathf.Sin(Mathf.Deg2Rad * GetComponent<Rigidbody2D>().rotation * (-1)), -speed * Mathf.Cos(Mathf.Deg2Rad * GetComponent<Rigidbody2D>().rotation * (-1)));
+            GetComponent<Animator>().SetBool("IsMoving", true);
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            GetComponent<Animator>().SetBool("IsMoving", false);
+        }
 
         if (Input.GetKey(KeyCode.RightArrow))
             GetComponent<Rigidbody2D>().rotation -= 5;
