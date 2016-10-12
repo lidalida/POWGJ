8f93977ff8da3b58f140c1b34ff6df34e8b2f3a2
lidalida <lidka.wojciechowska1996@gmail.com>
2016-10-12 19:09:58 +0200

    koniec vol. 2

---
+++ POWGJ/Assets/HotFix.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HotFix : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        if (!GetComponent<PlayerController>().enabled)
+            if (Input.GetKeyDown(KeyCode.Escape))
+                Application.LoadLevel("StartScene");
+	}
+}
